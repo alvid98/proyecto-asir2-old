412bd29565aa71d949f0fc9dac23b4dc665ce96c
alvid98 <alvid.1998@gmail.com>
2021-01-24 18:02:04 +0100

    actualizacion_pact

--- scripts/pact.sh
+++ scripts/pact.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 clear
 echo "Lista de paquetes que se pueden actualizar: "
-apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "PROGRAM: $1 INSTALLED: $2 AVAILABLE: $3\n"}' | cut -d " " -f 1,2
-lineas=$(apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "PROGRAM: $1 INSTALLED: $2 AVAILABLE: $3\n"}' | wc -l )
+apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "PAQUETE: $1 INSTALADO: $2 DISPONIBLE: $3\n"}' | cut -d " " -f 1,2
+lineas=$(apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "."}' | wc -l )
 echo ""
 
 read -p "¿Desea actualizar "$lineas" paquete/s? (s/n): " si
